0457f40d234fe95746e51f51d436125fa39494d0
LZylstra <lindseyzylstra@gmail.com>
2020-04-03 20:23:18 -0700

    updates for deploying database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
-const app = require('./app')
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DATABASE_URL } = require("./config");
+
+const db = knex({
+  client: "pg",
+  connection: DATABASE_URL
+});
+
+app.set("db", db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
